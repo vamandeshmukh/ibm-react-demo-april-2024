79d79ca0362ed6b0670331ec83e1ab390a7e63b6
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-30 15:49:42 +0530

    react-demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+const App = () => {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+  return <h1>IBM React Demo App</h1>;
+  
+};
 export default App;
+
+
+// const App = () => {
+
+//   return (
+//     <div>
+//       <h1>IBM React Demo App</h1>
+//       <p>IBM ReactJS Demo Application</p>
+//     </div>
+//   );
+// };
+// export default App;
+
+
+
+// // modern js function component 
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>IBM React Demo App</h1>
+//       <p>IBM ReactJS Demo Application</p>
+//     </div>
+//   );
+// }
+// export default App;
+
+// old js function component 
+// function App() {
+//   return (
+//     <div>
+//       <h1>IBM React Demo App</h1>
+//       <p>IBM ReactJS Demo Application</p>
+//     </div>
+//   );
+// }
+// export default App;
+
+
+
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div class="App">
+//       <p>IBM React Demo App</p>
+//     </div>
+//   );
+// }
+// export default App;
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
