bb54bd0237ea76abb4732ef02aa009379f4e114c
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-05-02 10:52:31 +0530

    react-day-2

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,38 @@
+import { useState } from "react";
 
 const Login = () => {
+
+    const [loginData, setLoginData] = useState({ username: '', password: '' });
+
+    const handleLoginInput = (evt) => {
+        console.log(evt.target.value);
+        setLoginData({
+            ...loginData,
+            [evt.target.name]: evt.target.value
+        });
+    };
+
+    // sonu sonu 
+
+    const handleLoginSubmit = (evt) => {
+        evt.preventDefault();
+        alert('Logged in successfully!')
+
+    };
 
     return (
         <>
             <h1>Login Component</h1>
             <p>Login here</p>
-            <form>
+            <form onSubmit={handleLoginSubmit}>
+                <input type="text" name="username" value={loginData.firstName} onChange={handleLoginInput} />
+                <input type="password" name="password" value={loginData.password} onChange={handleLoginInput} />
+                <input type="submit" value="Login" />
             </form>
+            <>
+                <p>username that you are entering: {loginData.username} </p>
+                <p>password that you are entering: {loginData.password}</p>
+            </>
         </>
     );
 };
