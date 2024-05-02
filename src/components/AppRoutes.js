770f477be3b29387d200650cf44545ee00c4bbce
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-05-02 14:36:36 +0530

    react-day-2

--- src/components/AppRoutes.js
+++ src/components/AppRoutes.js
@@ -1,8 +1,9 @@
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Login";
 import Parent from "./Parent";
 import Employee from './Employee';
 import Home from "./Home";
+import Page404 from './Page404';
 
 const AppRoutes = () => {
 
@@ -10,11 +11,12 @@ const AppRoutes = () => {
         <>
             <BrowserRouter>
                 <Routes>
-                    <Route exact path="/" element={<Home />} />
                     <Route path="login" element={<Login />} />
                     <Route path="emp" element={<Employee />} />
                     <Route path="parent" element={<Parent />} />
-                    <Route exact path="/" element={<Login />} />
+                    <Route exact path="/" element={<Home />} />
+                    <Route path="*" element={<Page404 />} />
+
                 </Routes>
             </BrowserRouter>
 
---
+++ src/components/Page404.js
@@ -0,0 +1,13 @@
+
+const Page404 = () => {
+
+    return (
+        <>
+            <h1>Page Not Found</h1>
+
+
+        </>
+    );
+};
+
+export default Page404;
