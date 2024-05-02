e7557982d6d64563199a13dab9d8e59631f32122
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-05-02 14:50:09 +0530

    react-day-2

--- src/components/Menubar.js
+++ src/components/Menubar.js
@@ -4,16 +4,12 @@ const Menubar = () => {
 
     return (
         <>
-            <h1>Menubar Component</h1>
-
             <ul>
                 <li> <Link to={'/'}>Home</Link> </li>
                 <li> <Link to={'/emp'}>Employee</Link> </li>
                 <li> <Link to={'/parent'}>Parent</Link> </li>
                 <li> <Link to={'/login'}>Login</Link> </li>
             </ul>
-
-
         </>
     );
 };
